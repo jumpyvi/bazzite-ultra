2be43f95cb374070d020611961571316d40e02c4
JumpyVi <62212220+jumpyvi@users.noreply.github.com>
2025-04-19 09:55:09 -0400

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,19 +2,6 @@
 
 set -ouex pipefail
 
-tee /etc/yum.repos.d/ghostty.repo <<'EOF'
-[copr:copr.fedorainfracloud.org:pgdev:ghostty]
-name=Copr repo for Ghostty owned by pgdev
-baseurl=https://download.copr.fedorainfracloud.org/results/pgdev/ghostty/fedora-$releasever-$basearch/
-type=rpm-md
-skip_if_unavailable=True
-gpgcheck=1
-gpgkey=https://download.copr.fedorainfracloud.org/results/pgdev/ghostty/pubkey.gpg
-repo_gpgcheck=0
-enabled=1
-enabled_metadata=1
-EOF
-
 tee /etc/yum.repos.d/docker-ce.repo <<'EOF'
 [docker-ce-stable]
 name=Docker CE Stable - $basearch
@@ -36,7 +23,7 @@ EOF
 RELEASE="$(rpm -E %fedora)"
 
 PACKAGES_TO_INSTALL=(
-    ghostty
+    foot
     nordvpn
     bridge-utils
     xhost
